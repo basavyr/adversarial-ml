bcbef002dbd318fab1030ce703b86ce7bc30f05b
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-08-29 15:42:17 +0300

    add fgsm attack
    source: https://pytorch.org/tutorials/beginner/fgsm_tutorial.html

---
+++ src/fgsm-attack/main.py
@@ -0,0 +1,197 @@
+# this project is created based on the official pytorch tutorial
+# source: https://pytorch.org/tutorials/beginner/fgsm_tutorial.html
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import numpy as np
+import matplotlib.pyplot as plt
+
+import time
+
+seed = 1137
+torch.manual_seed(seed)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+def get_device():
+    if torch.cuda.is_available():
+        _device = "cuda"
+    elif torch.backends.mps.is_available():
+        _device = "mps"
+    else:
+        _device = "cpu"
+
+    return torch.device(_device)
+
+
+# FGSM attack code
+def fgsm_attack(image, epsilon, data_grad):
+    # Collect the element-wise sign of the data gradient
+    sign_data_grad = data_grad.sign()
+    # Create the perturbed image by adjusting each pixel of the input image
+    perturbed_image = image + epsilon*sign_data_grad
+    # Adding clipping to maintain [0,1] range
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+    # Return the perturbed image
+    return perturbed_image
+
+
+# restores the tensors to their original scale
+def denorm(batch, device, mean=[0.1307], std=[0.3081]):
+    """
+    Convert a batch of tensors to their original scale.
+
+    Args:
+        batch (torch.Tensor): Batch of normalized tensors.
+        mean (torch.Tensor or list): Mean used for normalization.
+        std (torch.Tensor or list): Standard deviation used for normalization.
+
+    Returns:
+        torch.Tensor: batch of tensors without normalization applied to them.
+    """
+    if isinstance(mean, list):
+        mean = torch.tensor(mean).to(device)
+    if isinstance(std, list):
+        std = torch.tensor(std).to(device)
+
+    return batch * std.view(1, -1, 1, 1) + mean.view(1, -1, 1, 1)
+
+
+def test(model, device, test_loader, epsilon):
+
+    # Accuracy counter
+    correct = 0
+    adv_examples = []
+
+    # Loop over all examples in test set
+    for data, target in test_loader:
+
+        # Send the data and label to the device
+        data, target = data.to(device), target.to(device)
+
+        # Set requires_grad attribute of tensor. Important for Attack
+        data.requires_grad = True
+
+        # Forward pass the data through the model
+        output = model(data)
+        # get the index of the max log-probability
+        init_pred = output.max(1, keepdim=True)[1]
+
+        # If the initial prediction is wrong, don't bother attacking, just move on
+        if init_pred.item() != target.item():
+            continue
+
+        # Calculate the loss
+        loss = F.nll_loss(output, target)
+
+        # Zero all existing gradients
+        model.zero_grad()
+
+        # Calculate gradients of model in backward pass
+        loss.backward()
+
+        # Collect ``datagrad``
+        data_grad = data.grad.data
+
+        # Restore the data to its original scale
+        data_denorm = denorm(data, device)
+
+        # Call FGSM Attack
+        perturbed_data = fgsm_attack(data_denorm, epsilon, data_grad)
+
+        # Reapply normalization
+        perturbed_data_normalized = transforms.Normalize(
+            (0.1307,), (0.3081,))(perturbed_data)
+
+        # Re-classify the perturbed image
+        output = model(perturbed_data_normalized)
+
+        # Check for success
+        # get the index of the max log-probability
+        final_pred = output.max(1, keepdim=True)[1]
+        if final_pred.item() == target.item():
+            correct += 1
+            # Special case for saving 0 epsilon examples
+            if epsilon == 0 and len(adv_examples) < 5:
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append(
+                    (init_pred.item(), final_pred.item(), adv_ex))
+        else:
+            # Save some adv examples for visualization later
+            if len(adv_examples) < 5:
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append(
+                    (init_pred.item(), final_pred.item(), adv_ex))
+
+    # Calculate final accuracy for this epsilon
+    final_acc = correct/float(len(test_loader))
+    print(
+        f"Epsilon: {epsilon}\tTest Accuracy = {correct} / {len(test_loader)} = {final_acc}")
+
+    # Return the accuracy and an adversarial example
+    return final_acc, adv_examples
+
+
+def attack(model_path: str, device: torch.device):
+    epsilons = [0, .05, .1, .15, .2, .25, .3]
+    pretrained_model = model_path
+
+    # MNIST Test dataset and dataloader declaration
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, download=True, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,)),
+        ])),
+        batch_size=1, shuffle=True)
+
+    # Initialize the network
+    model = Net().to(device)
+
+    # Load the pretrained model
+    model.load_state_dict(torch.load(
+        pretrained_model, map_location=device, weights_only=True))
+
+    # Set the model in evaluation mode. In this case this is for the Dropout layers
+    model.eval()
+
+    accuracies = []
+    examples = []
+
+    # Run test for each epsilon
+    for eps in epsilons:
+        acc, ex = test(model, device, test_loader, eps)
+        accuracies.append(acc)
+        examples.append(ex)
+
+
+if __name__ == "__main__":
+    pretrained_model = "../mnist/lenet_mnist_model.pth.pt"
+    attack(pretrained_model, get_device())
