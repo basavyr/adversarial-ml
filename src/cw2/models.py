395a44fb7b46051c215238335c4e5e8ae0d117e1
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-06-14 16:14:47 +0300

    add new resnet with custom activation

--- src/cw2/models.py
+++ src/cw2/models.py
@@ -63,7 +63,7 @@ class CONV5_Net(nn.Module):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, activation_fn=F.tanh):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
@@ -71,6 +71,7 @@ class BasicBlock(nn.Module):
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
+        self.activation_fn = activation_fn
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -81,17 +82,17 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.tanh(self.bn1(self.conv1(x)))
+        out = self.activation_fn(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
-        out = F.tanh(out)
+        out = self.activation_fn(out)
         return out
 
 
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, activation_fn=F.tanh):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -101,6 +102,7 @@ class Bottleneck(nn.Module):
         self.conv3 = nn.Conv2d(planes, self.expansion *
                                planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        self.activation_fn = activation_fn
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -111,38 +113,44 @@ class Bottleneck(nn.Module):
             )
 
     def forward(self, x):
-        out = F.tanh(self.bn1(self.conv1(x)))
-        out = F.tanh(self.bn2(self.conv2(out)))
+        out = self.activation_fn(self.bn1(self.conv1(x)))
+        out = self.activation_fn(self.bn2(self.conv2(out)))
         out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
-        out = F.tanh(out)
+        out = self.activation_fn(out)
         return out
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, activation_fn=F.tanh):
         super(ResNet, self).__init__()
         self.in_planes = 64
+        self.activation_fn = activation_fn
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer1 = self._make_layer(
+            block, 64, num_blocks[0], stride=1, activation_fn=activation_fn)
+        self.layer2 = self._make_layer(
+            block, 128, num_blocks[1], stride=2, activation_fn=activation_fn)
+        self.layer3 = self._make_layer(
+            block, 256, num_blocks[2], stride=2, activation_fn=activation_fn)
+        self.layer4 = self._make_layer(
+            block, 512, num_blocks[3], stride=2, activation_fn=activation_fn)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride, activation_fn):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes,
+                          stride, activation_fn=activation_fn))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.tanh(self.bn1(self.conv1(x)))
+        out = self.activation_fn(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
@@ -153,5 +161,6 @@ class ResNet(nn.Module):
         return out
 
 
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+# Original ResNet18 factory function (uses F.tanh)
+def ResNet18(act_fn=F.tanh):
+    return ResNet(BasicBlock, [2, 2, 2, 2], activation_fn=act_fn)
